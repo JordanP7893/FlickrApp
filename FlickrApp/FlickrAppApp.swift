6cbd7f6d712f27d4645cb4511786475eeac684e0
JordanP7893 <65076724+JordanP7893@users.noreply.github.com>
2024-07-01 18:54:28 +0100

    Initial Commit

---
+++ FlickrApp/FlickrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrAppApp.swift
+//  FlickrApp
+//
+//  Created by Jordan Porter on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlickrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
